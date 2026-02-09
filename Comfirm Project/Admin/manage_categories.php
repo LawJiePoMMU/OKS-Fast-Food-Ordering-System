db20447457c7162045208da051f49b4f453643f2
LawJiePoMMU <LAW.JIE.PO@student.mmu.edu.my>
2026-02-09 10:06:51 +0800

    Update manage_categories.php

--- Comfirm Project/Admin/manage_categories.php
+++ Comfirm Project/Admin/manage_categories.php
@@ -50,7 +50,7 @@ $result_cat = mysqli_query($conn, "SELECT * FROM categories ORDER BY category_id
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>OKS Admin</title>
+    <title> Manage Categories - OKS Admin </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -183,5 +183,6 @@ $result_cat = mysqli_query($conn, "SELECT * FROM categories ORDER BY category_id
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="Admin java script folder/manage_categories.js"></script>
 </body>
+
 
 </html>
