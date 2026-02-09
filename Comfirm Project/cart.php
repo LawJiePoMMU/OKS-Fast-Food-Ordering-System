9d2bc5a73cb883922190c1fc0a311f6a68ce43e0
LawJiePoMMU <LAW.JIE.PO@student.mmu.edu.my>
2026-02-09 10:10:21 +0800

    Fix HTML encoding and update cart page title

--- Comfirm Project/cart.php
+++ Comfirm Project/cart.php
@@ -46,7 +46,7 @@ $grand_total = 0;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OKS Fast Food</title>
+    <title>Shopping Cart - OKS Fast Food</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="css folder/cart.css">
@@ -147,5 +147,6 @@ $grand_total = 0;
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="java script folder/cart.js"></script>
 </body>
+
 
 </html>
