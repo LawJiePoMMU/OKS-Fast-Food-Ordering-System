962b3ca3fca2623c1b7f06fe4765bb45db9d87be
LawJiePoMMU <LAW.JIE.PO@student.mmu.edu.my>
2026-02-09 10:24:13 +0800

    Update my_orders.php

--- Comfirm Project/my_orders.php
+++ Comfirm Project/my_orders.php
@@ -46,7 +46,7 @@ $result_orders = mysqli_query($conn, $sql_active_orders);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Active Orders - OKS Fast Food</title>
+    <title>My Orders - OKS Fast Food</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="css folder/my_orders.css">
@@ -178,5 +178,6 @@ $result_orders = mysqli_query($conn, $sql_active_orders);
         <?php endif; ?>
     </script>
 </body>
+
 
 </html>
