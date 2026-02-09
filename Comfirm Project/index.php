2ac05ec1f82ec01e2346d3b0e1863ce46cb2a23f
LawJiePoMMU <LAW.JIE.PO@student.mmu.edu.my>
2026-02-09 10:11:12 +0800

    Update index.php

--- Comfirm Project/index.php
+++ Comfirm Project/index.php
@@ -16,7 +16,7 @@ $res_categories = mysqli_query($conn, $sql_categories);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home - OKS Fast Food</title>
+  <title>Home page - OKS Fast Food</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
   <link rel="stylesheet" href="css folder/index.css">
@@ -109,4 +109,5 @@ $res_categories = mysqli_query($conn, $sql_categories);
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script src="java script folder/index.js"></script>
 </body>
+
 </html>
